2f00ec49c00ed2babed34435af73eeb827d45893
Kevin Jiang <kevinjiang@google.com>
2018-02-20 22:05:18 -0800

    Add py3 to setup.py.

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     license='Apache 2.0',
     zip_safe=True,
